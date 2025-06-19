89d07314f0335f363176760c8da327c8f7708b6f
Ethan Doan <ethanboyz10@gmail.com>
2025-06-19 16:13:14 -0400

    New state fields

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,89 @@ use wasm_bindgen::prelude::*;
 
 // Where game state is stored
 pub struct State {
+    surface: wgpu::Surface<'static>,        // Draw to surface
+    device: wgpu::Device,                   // Queue to device
+    queue: wgpu::Queue,                     // Push commands to queue
+    config: wgpu::SurfaceConfiguration,     // Configure surface
+    is_surface_configured: bool,
     window: Arc<Window>,
 }
 
 impl State {
-    pub async fn new(window: Arc<Window>) -> anyhow::Result<Self> {
+    pub async fn new(window: Arc<Window>) -> anyhow::Result<State> {
+        let size = window.inner_size();
+
+        // Let the instance be a handle to our GPU
+        // BackendBit::PRIMARY => Vulkan + Metal + DX12 + WebGPU
+        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
+            #[cfg(not(target_arch = "wasm32"))]
+            backends: wgpu::Backends::PRIMARY,
+            #[cfg(target_arch = "wasm32")]
+            backends: wgpu::Backends::GL,
+            ..Default::default()
+        });
+
+        // 
+        let surface = instance.create_surface(window.clone()).unwrap();
+
+        // Iterate and choose a working adapter for the surface
+        /*
+        let adapter = instance
+        .enumerate_adapters(wgpu::Backends::all())
+        .filter(|adapter| {
+            adapter.is_surface_supported(&surface)
+        })
+        .next()
+        .unwrap() */
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::default(),
+                compatible_surface: Some(&surface),
+                force_fallback_adapter: false,
+            })
+            .await?;
+
+        let (device, queue) = adapter
+            .request_device(&wgpu::DeviceDescriptor {
+                label: None,
+                required_features: wgpu::Features::empty(),
+
+                // Limit for WebGL2 browser support
+                required_limits: if cfg!(target_arch = "wasm32") {
+                    wgpu::Limits::downlevel_webgl2_defaults()
+                } else {
+                    wgpu::Limits::default()
+                },
+                memory_hints: Default::default(),
+                trace: wgpu::Trace::Off,
+            })
+            .await?;
+
+        let surface_caps = surface.get_capabilities(&adapter);
+                
+        let surface_format = surface_caps.formats.iter()
+            .find(|f| f.is_srgb())
+            .copied()
+            .unwrap_or(surface_caps.formats[0]);
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+
         Ok(Self {
+            surface,
+            device,
+            queue,
+            config,
+            is_surface_configured: false,
             window,
         })
     }
